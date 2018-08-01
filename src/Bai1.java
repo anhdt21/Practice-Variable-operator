d4fb7d2f4f3cd27ba4ac2400fd40f084445ddbc1
SON NGUYEN <myantj3m@gmail.com>
2018-08-01 09:35:55 +0700

    Add Bai1

---
+++ src/Bai1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static float nhapSo(String str) {
+        float x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(str);
+        x = sc.nextFloat();
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nhập điểm 3 môn của sinh viên");
+        float a = nhapSo("Vật lý :");
+        float b = nhapSo("Hóa học :");
+        float c = nhapSo("Sinh học :");
+        float average = (a + b + c)/3;
+        System.out.println("Tổng điểm là : " + (a+b+c));
+        System.out.println("Điểm trung bình cộng là : "+ (float)Math.round(average*100)/100 );
+    }
+}
